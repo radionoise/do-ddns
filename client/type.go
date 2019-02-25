1ddf29647af806ad781ca81e23269bf3776ea944
Daniil Molchanov <radionoise@gmail.com>
2019-02-25 16:01:57 +0400

    Added string methods

--- client/type.go
+++ client/type.go
@@ -15,6 +15,10 @@ type Domain struct {
 	Name string `json:"name"`
 }
 
+func (d Domain) String() string {
+	return fmt.Sprintf("{name: %v}", d.Name)
+}
+
 type DomainResponse struct {
 	Domain Domain `json:"domain"`
 }
@@ -28,6 +32,10 @@ type DomainRecord struct {
 	Type   string `json:"type"`
 	Name   string `json:"name"`
 	IpAddr string `json:"data"`
+}
+
+func (r DomainRecord) String() string {
+	return fmt.Sprintf("{id: %v, type: %v, name: %v, ip: %v}", r.Id, r.Type, r.Name, r.IpAddr)
 }
 
 type DomainRecordResponse struct {
